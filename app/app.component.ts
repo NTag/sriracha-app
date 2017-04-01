ad8975995e32fe7360456ab4417f992c7126fa75
Basile Bruneau <basilebruneau@gmail.com>
2017-04-01 14:54:20 +0100

    Add a button for Monzo connection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from "@angular/core";
 import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
+import { openUrl } from 'utils/utils';
 
 @Component({
   selector: "my-app",
-  template: `
-    <ActionBar title="My App"></ActionBar>
-    <!-- Your UI components go here -->
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   constructor() {
-      handleOpenURL((appURL: AppURL) => {
-        console.log('Got the following appURL', appURL);
-      });
-    }
+    handleOpenURL((appURL: AppURL) => {
+      console.log('Got the following appURL', appURL);
+    });
+  }
+
+  monzoConnect() {
+    openUrl('https://auth.getmondo.co.uk/?client_id=oauthclient_00009IxQz65UhA8dG2z4Sn&redirect_uri=http://google.com&response_type=code&state=randomSTRING');
+  }
 }
