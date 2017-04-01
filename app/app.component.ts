772be89aed960dea18ce9675529f7ea40c5cbb69
Basile Bruneau <basilebruneau@gmail.com>
2017-04-01 15:02:40 +0100

    Center the Monzo connect button

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
 import { openUrl } from 'utils/utils';
 
 @Component({
-  selector: "my-app",
-  templateUrl: './app.component.html'
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor() {
