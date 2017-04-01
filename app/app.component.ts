fdebb83a1441a97207855c482e0a207470d45abd
Basile Bruneau <basilebruneau@gmail.com>
2017-04-01 13:54:41 +0100

    Register a custom url scheme helpmebudget:// for oauth redirection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
 
 @Component({
   selector: "my-app",
@@ -8,5 +9,9 @@ import { Component } from "@angular/core";
   `
 })
 export class AppComponent {
-  // Your TypeScript logic goes here
+  constructor() {
+      handleOpenURL((appURL: AppURL) => {
+        console.log('Got the following appURL', appURL);
+      });
+    }
 }
