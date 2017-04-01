2f535d72e1a86264c6ff0795eb1cf65c94dee7bb
Basile Bruneau <basilebruneau@gmail.com>
2017-04-01 21:47:38 +0100

    Create budget page

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,12 +1,15 @@
 import { LoginComponent } from "./login.component";
 import { SetupComponent } from "./setup.component";
+import { BudgetComponent } from "./budget.component";
 
 export const routes = [
   { path: "", component: LoginComponent },
-  { path: "setup", component: SetupComponent }
+  { path: "setup", component: SetupComponent },
+  { path: "budget", component: BudgetComponent }
 ];
 
 export const navigatableComponents = [
   LoginComponent,
-  SetupComponent
+  SetupComponent,
+  BudgetComponent
 ];
---
+++ app/budget.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { UserService } from "./user.service";
+import { Router } from "@angular/router";
+
+
+@Component({
+  selector: 'budget',
+  templateUrl: './budget.component.html',
+  styleUrls: ['./budget.component.css']
+})
+export class BudgetComponent {
+  constructor(private userService: UserService, private router: Router) {
+
+  }
+
+}
