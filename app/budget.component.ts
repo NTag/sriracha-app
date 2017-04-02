a0f6db66143892c4b8e0cc1bb96374a9a3de246b
Basile Bruneau <basilebruneau@gmail.com>
2017-04-02 02:09:18 +0100

    Display a countdown when the budget is active

--- app/budget.component.ts
+++ app/budget.component.ts
@@ -12,9 +12,33 @@ export class BudgetComponent {
   amount:number = 30;
   end:Date;
   userInfos;
+  activeBudget;
 
   constructor(private userService: UserService, private router: Router) {
 
+  }
+
+  launchBudget() {
+    function zeros(n:number):string {
+      if (n === 0) {
+        return '00';
+      } else if (n < 10) {
+        return '0' + n;
+      } else {
+        return '' + n;
+      }
+    }
+    let updateTimeRemaining = () => {
+      let now = new Date();
+      let diff = ((this.activeBudget.end.getHours() + 24)*3600 + this.activeBudget.end.getMinutes()*60 - now.getHours()*3600 - now.getMinutes()*60 - now.getSeconds()) % (24*3600);
+      let remaining = '';
+      remaining += zeros(Math.floor(diff/3600)) + ':';
+      remaining += zeros(Math.floor((diff % 3600)/60)) + ':';
+      remaining += zeros(diff % 60);
+      this.activeBudget.remaining = remaining;
+    };
+
+    setInterval(updateTimeRemaining, 1000);
   }
 
   tabChange() {
@@ -40,6 +64,9 @@ export class BudgetComponent {
     }
 
     this.userService.addBudget(this.amount, d).subscribe(() => {}, () => {});
+
+    this.activeBudget = { amount: this.amount, end: d, remaining: '' };
+    this.launchBudget();
   }
 
 }
